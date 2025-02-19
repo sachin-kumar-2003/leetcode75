065cc7b0482a25928a8f1e7ab0fa903a926e01ba
sachin kumar <goputen215@gmail.com>
2025-02-19 22:29:37 +0530

    update

--- 16_longestOnes.py
+++ 16_longestOnes.py
@@ -5,6 +5,7 @@ Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
 Output: 6
 Explanation: [1,1,1,0,0,1,1,1,1,1,1]
 Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.'''
+from typing import List
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         ans=0
